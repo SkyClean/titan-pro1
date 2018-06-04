ce2c76f2b2754b81ecc379006fcc5afa7c6adb16
ihor <krylro@gmail.com>
2018-06-04 23:31:23 +0800

    update

---
+++ resources/app/js/bitcoin0.js
@@ -0,0 +1,278 @@
+var Datastore = require('nedb');
+var bip39 = require('bip39');
+try {
+  crypto = require('crypto');
+} catch (err) {
+  console.log('crypto support is disabled!');
+}
+var bitcoin = require('bitcoinjs-lib');
+
+var Hasher = [];
+Hasher.Salt = 'titan-wallet';
+Hasher.Algorithm = 'sha512';
+Hasher.Encryption = 'aes-256-cbc';
+Hasher.Encoding = {
+    In: 'utf8',
+    Out: 'hex'
+};
+
+var BITCOIN_CONSTANTS = {
+    Bitcoin: {
+        Decimals: 8,
+        Satoshis: 100000000,
+        Networks: {
+            Testnet: 'testnet',
+            Bitcoin: 'bitcoin',
+        }
+    },
+    ReturnValues: {
+        TransactionSubmitted: 'Transaction Submitted',
+        NoFreeOutputs: 'No free outputs to spend',
+        Fragments: {
+            MinimumFeeNotMet: 'min relay fee not met',
+        },
+    },
+    Messages: {
+        Wallet: {
+            Created: 'Your wallet has been created and saved!',
+            Mnemonic: 'Store this sequence safely',
+            Failed: 'A wallet could not be created at this moment',
+        },
+        Transactions: {
+            NOTSent: 'Transaction could not be sent',
+            Sent: 'Your transaction was sent'
+        },
+        Errors: {
+            FeeNotMet: 'A fee to process this transaction was not provided'
+        }
+    }
+};
+
+
+function hash(password) {
+
+    return new Promise((resolve, reject) => {
+
+        if (!password) reject('No value provided');
+
+        crypto.pbkdf2(password, "titan", 2048, 48, Hasher.Algorithm, (err, data) => {
+            if (err) reject(err);
+
+            const hex = data.toString('hex');
+            resolve(hex);
+
+        });
+    });
+}
+
+function encrypt(key, password) {
+    const cipher = crypto.createCipher(Hasher.Encryption, password);
+    return cipher.update(key, Hasher.Encoding.In, Hasher.Encoding.Out) + cipher.final(Hasher.Encoding.Out);
+}
+
+function decrypt(key, password) {
+    const cipher = crypto.createDecipher(Hasher.Encryption, password);
+    return cipher.update(key, Hasher.Encoding.Out, Hasher.Encoding.In) + cipher.final(Hasher.Encoding.In);
+}
+var utxos = null;
+function updateWallet(address){
+  var c_blockexplorer = require('blockchain.info/blockexplorer');
+  var c_exchange = require('blockchain.info/exchange');
+
+  c_blockexplorer.getUnspentOutputs(address).then((result) => {
+    utxos = result.unspent_outputs;
+    coins = result.unspent_outputs.reduce((a, c) => a + c.value, 0) / BITCOIN_CONSTANTS.Bitcoin.Satoshis;
+    console.log('coins', coins);
+    c_exchange.getTicker({ currency: 'USD' }).then( (r) => {
+      $total_usd = coins * r.sell;
+      $('#litecoin_balance_usd').html($total_usd.toFixed(2));
+    });
+
+    $('#litecoin_balance').html(coins.toFixed(8));
+    $('#litecoin_qr_code').html('');
+    $('#litecoin_qr_code').qrcode({width: 180,height: 180, text: address});
+
+  },(e) => {
+      if (e.toString() === BITCOIN_CONSTANTS.ReturnValues.NoFreeOutputs) {
+          $('#litecoin_balance').html('0.00');
+          $('#litecoin_qr_code').html('');
+          $('#litecoin_qr_code').qrcode({width: 180,height: 180, text: address});
+          console.log('error');
+      }
+  });
+
+  c_blockexplorer.getLatestBlock()
+      .then(block => c_blockexplorer.getBlock(block.hash))
+      .then((block) => {
+        console.log('block', block);
+        // Fee Normal for a single transaction (450 bytes)
+        txNormalFeeKB = 450;
+        txfeeperbyte = block.fee / block.size;
+        fee = txNormalFeeKB * txfeeperbyte / BITCOIN_CONSTANTS.Bitcoin.Satoshis;
+        $('#ltctxfee').val(fee.toFixed(8));
+      });
+}
+var litecoin_mywif  = null;
+var LTCWallet = [];
+
+LTCWallet.Defaults = {
+    Encryption: 'aes-256-cbc',
+    Path: "m/44'/0'/0'/0/0",
+    DBFileName: 'bitcoin',
+};
+
+LTCWallet.Events = {
+  Updated: 'updated',
+};
+
+$('document').ready(function(){
+
+
+  db = new Datastore({ filename: `./bitcoin.db`, autoload: true });
+
+  db.find({'network':'bitcoin'}, (err, docs) => {
+      if (err || docs.length == 0){
+        console.log(err);
+        console.log(docs);
+        const mnemonic = bip39.generateMnemonic();
+        const seed = bip39.mnemonicToSeed(mnemonic);
+        const master = bitcoin.HDNode.fromSeedBuffer(seed, bitcoin.networks.bitcoin);
+        console.log('wallet', BTCWallet);
+        const derived = master.derivePath(BTCWallet.Defaults.Path);
+        const address = derived.getAddress();
+        wif = derived.keyPair.toWIF();
+        network = 'bitcoin';
+        console.log(crypto);
+        var crypt = require('crypto');
+        const cipher = crypt.createCipher(BTCWallet.Defaults.Encryption, 'titan');
+        wif = cipher.update(wif, 'utf8', 'hex') + cipher.final('hex');
+        bitcoin_mywif = wif;
+        const obj = {
+            name: 'bitcoin',
+            address: address,
+            wif: wif,
+            network: network,
+        };
+        db.insert(obj);
+        $('#bitcoin_wallet_address').html(address);
+        updateWallet(address);
+        setInterval(updateWallet(address), 5000);
+      } else {
+        console.log('bitcoin address', docs[0].address);
+        $('#bitcoin_wallet_address').html(docs[0].address);
+        bitcoin_mywif = docs[0].wif;
+        updateWallet(docs[0].address);
+        setInterval(updateWallet(docs[0].address), 5000);
+      }
+  });
+
+
+
+  $('.bitcoin-control-img').hover(function(){
+    id = $(this).attr('id');
+    if (id == "bitcoin_copy_address"){
+      $(this).attr('src', '../images/ethereum_copy_address_hover.png');
+      $('#bitcoin-subtitle').html('COPY THIS ADDRESS!')
+    } else if (id == 'bitcoin_email_address'){
+      $(this).attr('src', '../images/ethereum_email_address_hover.png');
+      $('#bitcoin-subtitle').html('EMAIL THIS ADDRESS!')
+    } else {
+      $(this).attr('src', '../images/ethereum_explorer_address_hover.png');
+      $('#bitcoin-subtitle').html('VIEW ON BLOCKCHAIN!');
+    }
+  },function(){
+    id = $(this).attr('id');
+    $('#bitcoin-subtitle').html('YOUR BITCOIN ADDRESS!')
+    if (id == "bitcoin_copy_address"){
+      $(this).attr('src', '../images/ethereum_copy_address.png');
+    } else if (id == 'bitcoin_email_address'){
+      $(this).attr('src', '../images/ethereum_email_address.png');
+    } else {
+      $(this).attr('src', '../images/ethereum_explorer_address.png');
+    }
+  });
+
+  $('.bitcoin-control-img').click(function(){
+    id = $(this).attr('id');
+    address = $('#bitcoin_wallet_address').html();
+    if (id == "bitcoin_copy_address"){
+      CopyBitcoinAddress();
+      $('#bitcoin-subtitle').html('ADDRESS COPIED TO CLIPBOARD!');
+    } else if (id == 'bitcoin_email_address'){
+      var email = '';
+      var subject = 'Titanwallet BitCoin Address';
+      var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + "MY BITCOIN ADDRESS is: " + address;
+      win = window.open(mailto_link, 'emailWindow');
+      if (win && win.open && !win.closed) win.close();
+    } else if (id == 'bitcoin_explorer_address'){
+      shell.openExternal('https://blockchain.info/address'+address);
+    }
+  });
+})
+
+function CopyBitcoinAddress() {
+    address = $('#bitcoin_wallet_address').html();
+    clipboardy.writeSync(address);
+}
+
+
+function SendBitcoin(){
+      btc = $('#send_bitcoin_amount').val();
+      to_address = $('#send_bitcoin_to').val();
+      fee = $('#btctxfee').val();
+      fee = fee * BITCOIN_CONSTANTS.Bitcoin.Satoshis;
+      address = $('#bitcoin_wallet_address').html();
+      const satoshis = Math.round(btc * BITCOIN_CONSTANTS.Bitcoin.Satoshis);
+
+      const network = 'bitcoin';
+      var bitcoin = require('bitcoinjs-lib');
+      var txb = new bitcoin.TransactionBuilder(bitcoin.networks.bitcoin);
+
+      let current = 0;
+      for (const utx of utxos) {
+
+          txb.addInput(utx.tx_hash_big_endian, utx.tx_output_n);
+
+          current += utx.value;
+          if (current >= (satoshis + fee)) break;
+      }
+
+      console.log('bitcoin', to_address, satoshis);
+      txb.addOutput(to_address, satoshis);
+
+      const change = current - (satoshis + fee);
+      if (change) txb.addOutput(address, change);
+
+
+      const wif = readDecrypted(bitcoin_mywif);
+      console.log('wif', wif);
+      const key = bitcoin.ECPair.fromWIF(wif);
+
+      txb.sign(0, key);
+
+      const raw = txb.build().toHex();
+      pushtx = require('blockchain.info/pushtx');
+      c_pushtx = pushtx.pushtx;
+      $('.modal').modal('hide');
+      return c_pushtx(raw).then(result => {result === BITCOIN_CONSTANTS.ReturnValues.TransactionSubmitted; console.log(result);});
+}
+
+function readDecrypted(wif) {
+    var crypto = require('crypto');
+    const cipher = crypto.createDecipher(BTCWallet.Defaults.Encryption, 'titan');
+    return cipher.update(wif, 'hex', 'utf8') + cipher.final('utf8');
+}
+
+function CheckBitCoinAvailable(val){
+  console.log(val);
+  fee = $('#btctxfee').val();
+  balance = $('#bitcoin_balance').html();
+  total_amount = parseFloat(fee) + parseFloat(val);
+  console.log('balance', balance);
+  console.log('total_amount', total_amount);
+  if (parseFloat(total_amount) < parseFloat(balance)) {
+    $('#sendbtcbutton').prop('disabled', false);
+  } else {
+    $('#sendbtcbutton').prop('disabled', true);
+  }
+}
---
+++ resources/app/js/litecoin.js
@@ -0,0 +1,262 @@
+var Datastore = require('nedb');
+var bip39 = require('bip39');
+try {
+  crypto = require('crypto');
+} catch (err) {
+  console.log('crypto support is disabled!');
+}
+var litecoin = require('bitcoinjs-lib');
+
+var Hasher = [];
+Hasher.Salt = 'titan-wallet';
+Hasher.Algorithm = 'sha512';
+Hasher.Encryption = 'aes-256-cbc';
+Hasher.Encoding = {
+    In: 'utf8',
+    Out: 'hex'
+};
+
+var LITECOIN_CONSTANTS = {
+    Litecoin: {
+        Decimals: 8,
+        Satoshis: 100000000,
+        Networks: {
+            Testnet: 'testnet',
+            Bitcoin: 'bitcoin',
+            Litecoin: 'litecoin',
+        }
+    },
+    ReturnValues: {
+        TransactionSubmitted: 'Transaction Submitted',
+        NoFreeOutputs: 'No free outputs to spend',
+        Fragments: {
+            MinimumFeeNotMet: 'min relay fee not met',
+        },
+    },
+    Messages: {
+        Wallet: {
+            Created: 'Your wallet has been created and saved!',
+            Mnemonic: 'Store this sequence safely',
+            Failed: 'A wallet could not be created at this moment',
+        },
+        Transactions: {
+            NOTSent: 'Transaction could not be sent',
+            Sent: 'Your transaction was sent'
+        },
+        Errors: {
+            FeeNotMet: 'A fee to process this transaction was not provided'
+        }
+    }
+};
+
+
+function hash(password) {
+
+    return new Promise((resolve, reject) => {
+
+        if (!password) reject('No value provided');
+
+        crypto.pbkdf2(password, "titan", 2048, 48, Hasher.Algorithm, (err, data) => {
+            if (err) reject(err);
+
+            const hex = data.toString('hex');
+            resolve(hex);
+
+        });
+    });
+}
+
+var utxos = null;
+var ltcUSD = 0;
+function updateLTCWallet(address){
+  console.log('updateltcwallet', address);
+  $.get('https://api.blockcypher.com/v1/ltc/main/addrs/'+address+'/balance') .then(function(data) {
+    console.log('litecoin balance', data.balance);
+    ltc_balance = data.balance;
+    $('#litecoin_balance').html(ltc_balance.toFixed(8));
+    $('#litecoin_qr_code').html('');
+    $('#litecoin_qr_code').qrcode({width: 180,height: 180, text: address});
+
+    var api = "https://api.coinmarketcap.com/v1/ticker/litecoin/";
+    $.get(api, function(data, status){
+      ltcUSD = parseFloat(data[0]['price_usd']);
+      $total_usd =ltc_balance * ltcUSD;
+      $('#litecoin_balance_usd').html($total_usd.toFixed(2));
+    });
+  });
+
+  $.get('https://api.blockcypher.com/v1/btc/main').then(function(data) {
+    lastest_url = data.latest_url;
+    console.log('lastest_url', lastest_url);
+    $.get(lastest_url).then(function(blockdata) {
+      txNormalFeeKB = 450;
+      console.log(blockdata);
+      console.log('block', blockdata.fees, blockdata.size);
+      txfeeperbyte = blockdata.fees / blockdata.size;
+      fee = txNormalFeeKB * txfeeperbyte / LITECOIN_CONSTANTS.Litecoin.Satoshis;
+      $('#ltctxfee').val(fee.toFixed(8));
+    });
+  });
+}
+var litecoin_mywif  = null;
+var LTCWallet = [];
+
+LTCWallet.Defaults = {
+    Encryption: 'aes-256-cbc',
+    Path: "m/44'/0'/0'/0/0",
+    DBFileName: 'litecoin',
+};
+
+LTCWallet.Events = {
+  Updated: 'updated',
+};
+var address_lite = 0;
+$('document').ready(function(){
+  db_litecoin = new Datastore({ filename: `./litecoin.db`, autoload: true });
+  db_litecoin.find({'network':'litecoin'}, (err, docs) => {
+      if (err || docs.length == 0){
+        console.log(err);
+        console.log(docs);
+        const mnemonic = bip39.generateMnemonic();
+        const seed = bip39.mnemonicToSeed(mnemonic);
+        const master = litecoin.HDNode.fromSeedBuffer(seed, litecoin.networks.litecoin);
+        console.log('wallet', LTCWallet);
+        const derived = master.derivePath(LTCWallet.Defaults.Path);
+        address_lite = derived.getAddress();
+        wif = derived.keyPair.toWIF();
+        network = 'litecoin';
+        console.log(crypto);
+        var crypt = require('crypto');
+        const cipher = crypt.createCipher(LTCWallet.Defaults.Encryption, 'titan');
+        wif = cipher.update(wif, 'utf8', 'hex') + cipher.final('hex');
+        litecoin_mywif = wif;
+        const obj = {
+            name: 'litecoin',
+            address: address_lite,
+            wif: wif,
+            network: network,
+        };
+        db_litecoin.insert(obj);
+        $('#litecoin_wallet_address').html(address_lite);
+        updateLTCWallet(address_lite);
+        setInterval(updateLTCWallet(address_lite), 3000);
+      } else {
+        console.log('litecoin address', docs[0].address);
+        $('#litecoin_wallet_address').html(docs[0].address);
+        litecoin_mywif = docs[0].wif;
+        address_lite = docs[0].address;
+        updateLTCWallet(address_lite);
+        setInterval(updateLTCWallet(address_lite), 3000);
+      }
+  });
+
+
+
+  $('.litecoin-control-img').hover(function(){
+    id = $(this).attr('id');
+    if (id == "litecoin_copy_address"){
+      $(this).attr('src', '../images/ethereum_copy_address_hover.png');
+      $('#litecoin-subtitle').html('COPY THIS ADDRESS!')
+    } else if (id == 'litecoin_email_address'){
+      $(this).attr('src', '../images/ethereum_email_address_hover.png');
+      $('#litecoin-subtitle').html('EMAIL THIS ADDRESS!')
+    } else {
+      $(this).attr('src', '../images/ethereum_explorer_address_hover.png');
+      $('#litecoin-subtitle').html('VIEW ON BLOCKCHAIN!');
+    }
+  },function(){
+    id = $(this).attr('id');
+    $('#litecoin-subtitle').html('YOUR LITECOIN ADDRESS!')
+    if (id == "litecoin_copy_address"){
+      $(this).attr('src', '../images/ethereum_copy_address.png');
+    } else if (id == 'litecoin_email_address'){
+      $(this).attr('src', '../images/ethereum_email_address.png');
+    } else {
+      $(this).attr('src', '../images/ethereum_explorer_address.png');
+    }
+  });
+
+  $('.litecoin-control-img').click(function(){
+    id = $(this).attr('id');
+    address = $('#litecoin_wallet_address').html();
+    if (id == "litecoin_copy_address"){
+      CopyLitecoinAddress();
+      $('#litecoin-subtitle').html('ADDRESS COPIED TO CLIPBOARD!');
+    } else if (id == 'litecoin_email_address'){
+      var email = '';
+      var subject = 'Titanwallet Litecoin Address';
+      var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + "MY LITECOIN ADDRESS is: " + address;
+      win = window.open(mailto_link, 'emailWindow');
+      if (win && win.open && !win.closed) win.close();
+    } else if (id == 'litecoin_explorer_address'){
+      shell.openExternal('https://blockchain.info/address'+address);
+    }
+  });
+})
+
+function CopyLitecoinAddress() {
+    address = $('#litecoin_wallet_address').html();
+    clipboardy.writeSync(address);
+}
+
+
+function SendLitecoin(){
+      ltc = $('#send_litecoin_amount').val();
+      to_address = $('#send_litecoin_to').val();
+      fee = $('#ltctxfee').val();
+      fee = fee * LITECOIN_CONSTANTS.Litecoin.Satoshis;
+      address = $('#litecoin_wallet_address').html();
+      const satoshis = Math.round(ltc * LITECOIN_CONSTANTS.Litecoin.Satoshis);
+
+      const network = 'litecoin';
+      var litecoin = require('bitcoinjs-lib');
+      var txb = new litecoin.TransactionBuilder(litecoin.networks.litecoin);
+
+      let current = 0;
+      for (const utx of utxos) {
+
+          txb.addInput(utx.tx_hash_big_endian, utx.tx_output_n);
+
+          current += utx.value;
+          if (current >= (satoshis + fee)) break;
+      }
+
+      console.log('litecoin', to_address, satoshis);
+      txb.addOutput(to_address, satoshis);
+
+      const change = current - (satoshis + fee);
+      if (change) txb.addOutput(address, change);
+
+
+      const wif = readDecrypted(litecoin_mywif);
+      console.log('wif', wif);
+      const key = litecoin.ECPair.fromWIF(wif);
+
+      txb.sign(0, key);
+
+      const raw = txb.build().toHex();
+      pushtx = require('blockchain.info/pushtx');
+      c_pushtx = pushtx.pushtx;
+      $('.modal').modal('hide');
+      return c_pushtx(raw).then(result => {result === LITECOIN_CONSTANTS.ReturnValues.TransactionSubmitted; console.log(result);});
+}
+
+function readDecrypted(wif) {
+    var crypto = require('crypto');
+    const cipher = crypto.createDecipher(LTCWallet.Defaults.Encryption, 'titan');
+    return cipher.update(wif, 'hex', 'utf8') + cipher.final('utf8');
+}
+
+function CheckLiteCoinAvailable(val){
+  console.log(val);
+  fee = $('#ltctxfee').val();
+  balance = $('#litecoin_balance').html();
+  total_amount = parseFloat(fee) + parseFloat(val);
+  console.log('balance', balance);
+  console.log('total_amount', total_amount);
+  if (parseFloat(total_amount) < parseFloat(balance)) {
+    $('#sendltcbutton').prop('disabled', false);
+  } else {
+    $('#sendltcbutton').prop('disabled', true);
+  }
+}
